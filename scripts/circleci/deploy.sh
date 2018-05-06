8c700eb5c871eef5603ca8b41714a4f1c41af273
wsmidt <smidtwarner@gmail.com>
2018-05-06 16:33:14 -0700

    use docker compose up rather than start to have the container recreated with the new version

--- scripts/circleci/deploy.sh
+++ scripts/circleci/deploy.sh
@@ -12,4 +12,4 @@ fi
 #TODO: we really should have many versions here. right now theres one version so we have to do a
 #      build of all the services to get an updated version to deploy out.
 export SSERVICES_TAG_VERSION=$VERSION
-ecs-cli compose --project-name ping --region us-west-1 --cluster stupidservices --file $THIS_SCRIPT/files/docker-compose.yml service start
+ecs-cli compose --project-name ping --region us-west-1 --cluster stupidservices --file $THIS_SCRIPT/files/docker-compose.yml service up
