b096d2b1c39f9f925880bf5c220e671acc1ba49d
wsmidt <smidtwarner@gmail.com>
2018-05-06 18:04:08 -0700

    test

--- ping/scripts/container/publish.sh
+++ ping/scripts/container/publish.sh
@@ -2,4 +2,13 @@
 
 DOCKER_REPO=stupidservices/ping
 
-docker push $DOCKER_REPO:latest 
+VERSION=$1
+
+if [ -z $VERSION ]; then
+  echo "Usage: $0 VERSION"
+  echo "  example: $0 6669"
+  echo "    pushes the container $DOCKER_REPO:6669"
+  exit 1
+fi
+
+docker push $DOCKER_REPO:$VERSION
